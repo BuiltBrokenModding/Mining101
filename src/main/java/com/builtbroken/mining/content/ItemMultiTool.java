a64eea2be254430181e7cf23c46e1ed54abd889b
Robert S <rseifert.phone@gmail.com>
2015-02-14 04:06:33 -0500

    Added .iml to ignore list, Added mutlti tool and pulaski as future tool ideas

---
+++ src/main/java/com/builtbroken/mining/content/ItemMultiTool.java
@@ -0,0 +1,17 @@
+package com.builtbroken.mining.content;
+
+import com.builtbroken.mc.prefab.items.ItemAbstractTool;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemTool;
+
+/** Pick-ax, Ax, Shovel, and wrench all in one tool. Will automatically change texture to
+ * match the block it is pointing at. This way it informed the player that it can mine
+ * the material. If it can't mine the material it will default to a metal rod.
+ *
+ * As well requires upgrades in order to actually support all material types. The item
+ * can also be configured to mine materials with one tool vs another.
+ * Created by robert on 2/14/2015.
+ */
+public class ItemMultiTool extends ItemAbstractTool
+{
+}
---
+++ src/main/java/com/builtbroken/mining/content/ItemPulaski.java
@@ -0,0 +1,14 @@
+package com.builtbroken.mining.content;
+
+import com.builtbroken.mc.prefab.items.ItemAbstractTool;
+
+/** http://en.wikipedia.org/wiki/Pulaski_%28tool%29
+ * Combination of an axe and hoe. On right click can be used
+ * to til the ground. As well is very effective against
+ * any plant or wood based block
+ *
+ * Created by robert on 2/14/2015.
+ */
+public class ItemPulaski extends ItemAbstractTool
+{
+}
